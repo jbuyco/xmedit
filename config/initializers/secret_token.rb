f9cc2c9122ac73ca6740a1289665b7f84eb3010e
Jonathan Buyco <jbuyco@reinvent.com>
2013-07-30 18:19:25 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xmedit::Application.config.secret_token = 'a556613a4763595bf4b64787504b68c876bb2f2841087373a4677bc640b2ef6279011bf754eaa335f3d68baea5f6d5ccd72eca5e748db1857c27b5b790c8c673'
