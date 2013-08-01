d6e1953efe7a3cb632c8a4e8be6dc8bf9cf4abd9
Jonathan Buyco <jbuyco@reinvent.com>
2013-08-01 01:01:18 -0700

    Add styling (#5)
    * Separate header into partial
    * Add description
    * Enhance button disabling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def dark_icon(name)
+    content_tag("i", nil, class: "icon-#{name}")
+  end
+
   def icon(name)
     content_tag("i", nil, class: "icon-#{name} icon-white")
   end
